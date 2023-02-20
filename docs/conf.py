f328865ce7539064c04f686b64b62cb47d6a480a
Dexter Ty <dexter.gets@gmail.com>
2023-02-20 10:53:24 -0500

    package code ready for release

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspaht('.'))
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -22,7 +22,7 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.atuodoc',
+    'sphinx.ext.autodoc',
 ]
 
 templates_path = ['_templates']
